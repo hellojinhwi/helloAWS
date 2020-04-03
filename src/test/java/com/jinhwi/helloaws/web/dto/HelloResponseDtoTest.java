e35a0224bfe2a0dd86534acb886d421d65c9d488
hellojinhwi <hellojinhwi@gmail.com>
2020-04-03 22:19:10 +0900

    dto 단위테스트

---
+++ src/test/java/com/jinhwi/helloaws/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,21 @@
+package com.jinhwi.helloaws.web.dto;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트() {
+        //given
+        String name = "jinhwi";
+        int amount = 27;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}

3c9e53f8fd23417caaeeb3ec5c1ca684f48f5141
hellojinhwi <hellojinhwi@gmail.com>
2020-04-06 18:47:21 +0900

    JPA, 등록/수정/조회 API 생성 및 JPA Auditting

---
+++ src/main/java/com/jinhwi/helloaws/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.jinhwi.helloaws.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}

a38457d9bed7d7f0ad053ab409dbd6acf390ad45
hellojinhwi <hellojinhwi@gmail.com>
2020-04-07 22:49:07 +0900

    Mustache로 화면 구성하기

--- src/main/java/com/jinhwi/helloaws/service/posts/PostsService.java
+++ src/main/java/com/jinhwi/helloaws/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.jinhwi.helloaws.service.posts;
 
 import com.jinhwi.helloaws.domain.posts.Posts;
 import com.jinhwi.helloaws.domain.posts.PostsRepository;
+import com.jinhwi.helloaws.web.dto.PostsListResponseDto;
 import com.jinhwi.helloaws.web.dto.PostsResponseDto;
 import com.jinhwi.helloaws.web.dto.PostsSaveRequestDto;
 import com.jinhwi.helloaws.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
---
+++ src/main/java/com/jinhwi/helloaws/web/dto/PostsListResponseDto.java
@@ -0,0 +1,22 @@
+package com.jinhwi.helloaws.web.dto;
+
+import com.jinhwi.helloaws.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
